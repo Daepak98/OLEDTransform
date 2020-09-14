5309b821632637d4f51b7667b4a806428e4c83c5
daepak98 <bunch.of.food@gmail.com>
2020-09-14 19:43:41 -0400

    Updated algorithm to use sobel filter and am adding region labelling

--- transform.py
+++ transform.py
@@ -1,12 +1,15 @@
-from imageio import imread, imwrite
-from skimage import color
+import os
+import sys
+from multiprocessing import Pool
+from time import monotonic
+
 # from matplotlib.pyplot import imshow, axis
 import numpy as np
 import statistics as st
-import os
-from multiprocessing import Pool
-from time import monotonic
-    
+from imageio import imread, imwrite
+from skimage import color, feature, filters
+
+
 def detect_edges(image, sigma=None):
     edges = np.zeros(image.shape)
     for i, row in enumerate(image):
@@ -31,6 +34,14 @@ def highlight_points(vals, sigma=None):
             indices.append(i)
     return indices
 
+def decide_cutoff(row, threshold):
+    for i in range(len(row)):
+        row[i] = row[i] if row[i] > threshold else 0
+    return row
+
+def map_regions(image):
+    return ndimage.label(image)
+
 def do_all_tests():
     test_images = 'test_images/'
     image_paths = [test_images+path for path in os.listdir(test_images)]
@@ -44,18 +55,30 @@ def do_one(path):
     print("Starting ", path)
     im = imread(path)
     gray_im = color.rgb2gray(im)
+
     # outlined_im = detect_edges(gray_im, sigma=3.0)
-    outlined_im = detect_edges(gray_im) * 255
+    # outlined_im = detect_edges(gray_im) * 255
+    # outlined_im = feature.canny(gray_im) * 255
+    outlined_im = filters.sobel(gray_im)
     
+    mapped, num_regions = map_regions(outlined_im)
+
+    mapped = mapped
+
     output = path.split('/')[-1].split('.png')[0]
     output = 'output/'+output+'_output.png'
-    imwrite(output, outlined_im.astype('uint8'))
+    imwrite(output, region_mapped.astype('uint8'))
 
     print("Finished: ", path)
 
 if __name__ == "__main__":
     start = monotonic()
-    # do_all_tests()
-    do_one('test_images/'+'ricos_grappler.png')
+    args = sys.argv[1:]
+    if len(args) > 0:
+        for path in args:
+            do_one(path)
+    else:
+        # do_all_tests()
+        do_one('test_images/'+'journey.png')
     print("Finished All.")
     print("Elapsed Time ", (monotonic() - start)/60)
