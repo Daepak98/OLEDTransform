ad40e33f7813da93250dca0f984910f63df2e284
daepak98 <bunch.of.food@gmail.com>
2020-11-19 11:05:57 -0500

    Updated Model and training data - Tested w/new output

--- transform.py
+++ transform.py
@@ -1,5 +1,5 @@
 import os
-# import sys
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,11 +18,12 @@ def preprocess(input_dir, expected_dir):
     input_images = []
     expected_images = []
     files = os.listdir(input_dir)
-    limit = 2#len(files)
+    limit = len(files)
     for file in files[:limit]:
-        temp = color.rgba2rgb(imread(input_dir+file))
+        temp = imread(input_dir+file)
+        temp = color.rgba2rgb(imread(input_dir+file)) if temp.shape[-1] == 4 else temp
         input_images.append(temp)
-        temp = color.rgba2rgb(imread(expected_dir+file))
+        temp = imread(expected_dir+file)
         temp = color.rgb2gray(temp)
         expected_images.append(temp)
     
@@ -58,17 +59,22 @@ def train(x, y):
         pkl.dump(clf, out)
     return clf, X_test, Y_test
 
-def create_model_using_image(model, im, output):
-    new_im = np.zeros_like(im)
+def create_image_using_model(model, im, output):
+    temp = color.rgba2rgb(im) if im.shape[-1] == 4 else im
+    new_im = np.zeros_like(temp)
     # im_temp = im.reshape((im.shape[0]*im.shape[1], 3))
-    inds = np.array(list(np.ndindex(*im.shape[:-1])))
-    flat = im.flatten().reshape((-1, 3))
+    inds = np.array(list(np.ndindex(*temp.shape[:-1])))
+    flat = temp.flatten().reshape((-1, 3))
     samples = np.zeros((flat.shape[0], 5))
     samples[:, :2], samples[:, 2:] = inds, flat
     predictions = model.predict(samples)
     new_im = predictions.reshape(im.shape[:-1])
     scaled = (new_im*255).astype('uint8')
-    imwrite(output, scaled)
+    try:
+        imwrite(output, scaled, format=".png")
+    except OSError:
+        print("This doesn't have a good name. Writing to home dir")
+        imwrite("bad_name.png", scaled)
     return scaled
     
 
@@ -82,27 +88,45 @@ def plotimages(*ims, title=None):
         ax.imshow(im, cmap=cm.gray)
     plt.show()
 
-oled_folder = "input/oled/"
-rgb_folder = "input/rgb/"
-output_dir = "output/"
-model_path = "./oledify_model.pkl"
+if __name__ == "__main__":
+    oled_folder = "input/oled/" 
+    rgb_folder = "input/rgb/"
+    output_dir = "output/"
+    model_path = "./oledify_model.pkl"
 
-model = 0
-retrain = False
-if (not os.path.exists(model_path)) or retrain:
-    start = monotonic()
-    X, Y = preprocess(rgb_folder, oled_folder)
-    print("Preprocessing Time: ", monotonic() - start)
-    
-    start = monotonic()
-    model, x_test, Y_test = train(X, Y)
-    print("Training Time: ", monotonic() - start)
-else:
-    with open(model_path, 'rb') as f:
-        model = pkl.load(f)
+    model = 0
+    retrain = False
+    if (not os.path.exists(model_path)) or retrain:
+        print("Retraining Model")
+        start = monotonic()
+        X, Y = preprocess(rgb_folder, oled_folder)
+        print("Preprocessing Time: ", monotonic() - start)
         
-choice = "No Expectations.png"
-example = color.rgba2rgb(imread(rgb_folder+choice))
-start = monotonic()
-create_model_using_image(model, example, output_dir+choice)
-print("Writing Image Time: ", monotonic() - start)
+        start = monotonic()
+        model, x_test, Y_test = train(X, Y)
+        print("Training Time: ", monotonic() - start)
+    else:
+        with open(model_path, 'rb') as f:
+            model = pkl.load(f)
+            
+    
+    args = sys.argv[1:]
+    ims = []
+    for arg in args:
+        if (os.path.exists(arg) and os.path.isfile(arg)):
+            im = imread(arg)
+            plotimages(im, title=f"Original: {arg}")
+            ims.append((arg, im))
+        else:
+            print("Warning! Image at {} does not exist. Will process remaining that do exist.".format(arg))
+    if len(ims) == 0:
+        choice = "No Expectations.png"
+        example = color.rgba2rgb(imread(rgb_folder+choice))
+        ims.append((choice, example))
+    
+    for im in ims:
+        start = monotonic()
+        output_name = im[0].split('/')[-1]
+        output_im = create_image_using_model(model, im[1], output_dir+output_name)
+        plotimages(output_im, title="Processed Images")
+        print("Writing {} Time: ".format(output_name), monotonic() - start)
